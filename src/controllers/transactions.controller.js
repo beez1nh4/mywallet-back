4b41749ed88b7d5fe6f4f18cd2f324ebc792e9b6
Melissa <melmihomel@gmail.com>
2022-11-21 01:56:07 -0300

    feat: transaction controller find

--- src/controllers/transactions.controller.js
+++ src/controllers/transactions.controller.js
@@ -22,7 +22,18 @@ export async function addTransaction (req, res){
   };
 
 export async function findTransactions(req, res){
-    try{
-
-    }
+    try {
+        const session = await sessionsCollection.findOne({ token });
+        const user = await usersCollection.findOne({ _id: session?.userId });
+    
+        if (!user) {
+          return res.sendStatus(401);
+        }
+    
+        const transactions = await transactionsCollection.find().toArray();
+        res.send(transactions);
+      } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+      }
 }
